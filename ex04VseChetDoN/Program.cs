0d6081281e86ec7f82d3be99931daa75734b5c94
Arslan <arslan.chen@mail.ru>
2023-04-11 21:31:47 +0400

    ex04 N instead a -correction added

--- ex04VseChetDoN/Program.cs
+++ ex04VseChetDoN/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 Console.Write("Введите целое положительное число: ");
-int a = int.Parse(Console.ReadLine()!);
+int N = int.Parse(Console.ReadLine()!);
 int count = 2;
-while (count <= a)
+while (count <= N)
 {
     Console.Write(count + "|");
     count = count + 2;
