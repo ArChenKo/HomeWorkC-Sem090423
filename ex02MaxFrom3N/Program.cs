e8dc89b631fc2dce69b4a1e52317485375472563
Arslan <arslan.chen@mail.ru>
2023-04-11 17:52:20 +0400

    ex02 added

---
+++ ex02MaxFrom3N/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int[] arr = new int[3];
+int indar = 0;
+while (indar < 3)
+{
+    Console.Write("Введите следующее целое число: ");
+    arr[indar] = int.Parse(Console.ReadLine()!);
+    indar++;
+}
+int count = 0;
+int max = 0;
+while (count < 3)
+{
+    if (arr[count] > arr[max])
+    {
+        max = count;
+    }
+    count++;
+}
+Console.WriteLine("Итак, максимальное из введенных чисел: "+arr[max]);
